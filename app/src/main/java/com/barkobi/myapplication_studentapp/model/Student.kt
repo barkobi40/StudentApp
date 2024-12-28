be5c556809c05179e76dfad898910e16c665dddb
Bar Kobi <122670126+barkobi40@users.noreply.github.com>
2024-12-28 22:30:14 +0200

    added adapter file + student recycler activity + student view holder+xml files

---
+++ app/src/main/java/com/barkobi/myapplication_studentapp/model/Student.kt
@@ -0,0 +1,10 @@
+package com.barkobi.myapplication_studentapp.model
+
+data class Student(
+    val name : String,
+    val id : String,
+    val number : String,
+    val avatarURL  : String,
+    var isChecked : Boolean
+)
+
